b98416ae8e9469761bea2ada609a55f3e5f4904b
Laurence Qi <laurence.qi@outlook.com>
2021-09-12 19:07:47 +1000

    New build with minor change of text

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg'; // this is how the logo came into view
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        This is the header
+        I'm going to put the calculator below this
       </header>
     </div>
   );
